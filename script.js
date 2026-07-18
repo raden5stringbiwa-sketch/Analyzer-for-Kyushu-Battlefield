a7156251e9e38acda8e522851f2e81467a876de8
raden5stringbiwa-sketch <raden5stringbiwa@gmail.com>
2026-07-18 15:27:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,35 @@ imageInput.addEventListener("change", function () {
     reader.readAsDataURL(file);
 
 });
+const analyzeButton =
+    document.getElementById("analyzeButton");
+
+const result =
+    document.getElementById("result");
+
+analyzeButton.addEventListener("click", async function () {
+
+    if (!preview.src) {
+
+        alert("画像を選択してください");
+
+        return;
+
+    }
+
+    result.textContent =
+        "解析中・・・";
+
+    const data =
+        await Tesseract.recognize(
+
+            preview.src,
+
+            "eng"
+
+        );
+
+    result.textContent =
+        data.data.text;
+
+});
